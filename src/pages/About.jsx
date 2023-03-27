0fea342c288c60114b2183120c5b99f9e97a42df
Ankesh Chopde <acchopde84@gmail.com>
2023-03-27 20:50:55 +0530

    tech

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
     <Box id="about" className="about section" paddingTop={{base:"20px",md:"50px",lg:"60px"}} style={{backgroundColor:"#152242",height:"400px"}}>
     <Stack spacing="14" align="center">
     <Heading  className="heading">About Me</Heading>
-     <Text id="user-detail-intro" fontSize={{base:"lg",md:"xl",lg:"xl"}} textAlign={{base:"center",md:"center",lg:"left"}} m="auto" marginTop={{base:"50px",md:"180px",lg:"200px"}} w="80%"  color={'gray.500'}>Passionate about coding and eager to work with smart people in a challenging environment.Proficient in MERN(MongoDB, ExpressJS, ReactJS, HTML, CSS, JavaScript, NodeJS) and Having exceptional interpersonal skills as well as the ability to produce interactions that organizations and people really care about. 
+     <Text id="user-detail-intro" fontSize={{base:"lg",md:"xl",lg:"xl"}} textAlign={{base:"center",md:"center",lg:"left"}} m="auto" marginTop={{base:"50px",md:"180px",lg:"200px"}} w="80%"  color={'gray.500'}>Passionate about coding and eager to work with smart people in a challenging environment.Proficient in MongoDB, ExpressJS, ReactJS, HTML, CSS, JavaScript, NodeJS and Having exceptional interpersonal skills as well as the ability to produce interactions that organizations and people really care about. 
       Love technology and try to use them smartly to make everyday tasks easier.</Text>
     </Stack>
     </Box>
